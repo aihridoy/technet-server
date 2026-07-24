718b328dd3beecae0b553ed4eff449dfc3a67023
aihridoy <cse_1912020168@lus.ac.bd>
2026-07-24 20:02:13 +0600

    refactor: extract testable app.js, allow MONGO_URI override

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-const express = require("express");
-const cors = require("cors");
+const app = require("./app");
 const { connectDB } = require("./utils/db");
-const productRoutes = require("./routes/productRoutes");
-const userRoutes = require("./routes/userRoutes");
-const orderRoutes = require("./routes/orderRoutes");
-
-const app = express();
 const { port } = require("./utils/config");
 
-app.use(cors());
-app.use(express.json());
-
 connectDB().catch(console.error);
-
-app.use(productRoutes);
-app.use(userRoutes);
-app.use(orderRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pfan7vt.mongodb.net/?retryWrites=true&w=majority`;
+const uri =
+  process.env.MONGO_URI ||
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pfan7vt.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -15,7 +17,7 @@ let db;
 const connectDB = async () => {
   try {
     await client.connect();
-    db = client.db("tech-net");
+    db = client.db(process.env.DB_NAME || "tech-net");
   } catch (err) {
     console.error("Database connection failed:", err);
     process.exit(1);
